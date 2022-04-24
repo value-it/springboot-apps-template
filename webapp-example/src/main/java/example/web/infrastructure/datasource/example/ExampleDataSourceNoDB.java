948d1f9595da2951b5d4aadacc0fe6a9ab788f75
value-urano <urano@value-it.co.jp>
2022-04-24 17:10:41 +0900

    デフォルトはDB接続無しに

--- webapp-example/src/main/java/example/web/infrastructure/datasource/example/ExampleDataSourceNoDB.java
+++ webapp-example/src/main/java/example/web/infrastructure/datasource/example/ExampleDataSourceNoDB.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@Profile("without_db")
+@Profile("default")
 public class ExampleDataSourceNoDB implements ExampleRepository {
 
     @Override
